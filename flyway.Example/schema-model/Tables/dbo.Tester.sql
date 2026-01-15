a8b3bca8e7934e830a7c77598e3ce905f09956e1
Bogdanski <jason.bogdanski@accenture.com>
2026-01-14 22:36:30 -0500

    Added flyway migrations

---
+++ flyway.Example/schema-model/Tables/dbo.Tester.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Tester]
+(
+[TesterId] [int] NULL,
+[Name] [nchar] (10) NULL
+)
+GO
---
+++ flyway.Example/schema-model/Tables/dbo.User.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[User]
+(
+[UserId] [int] NOT NULL IDENTITY(1, 1),
+[Name] [nchar] (10) NOT NULL
+)
+GO
